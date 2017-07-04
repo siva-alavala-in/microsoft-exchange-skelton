96f4df22382165d7e91590bcf21060f0732871f0
Chenna Reddy <chenna.alavala@ing.nl>
2017-07-04 23:26:33 +0200

    initial version

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+
+import microsoft.exchange.webservices.data.core.ExchangeService;
+
+public class App
+{
+    ExchangeService exchangeService;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
